fbbcc4a177d072c7dfe069aa0a55929aed87f25c
David Jimenez <david.jimenez@rmit.edu.au>
2017-05-30 13:01:48 +1000

    added readme and updated members of the address book

---
+++ readme.md
@@ -0,0 +1,8 @@
+# Address book Servlet 3.1 and jstl practice
+
+Simple web app demonstrating the use of jstl and Servlet 3.1 spec.
+
+Managed with maven.
+
+## build 
+```$mvn clean package```
--- src/main/java/net/runnerdave/controller/ListServlet.java
+++ src/main/java/net/runnerdave/controller/ListServlet.java
@@ -28,24 +28,23 @@ public class ListServlet extends HttpServlet {
     static {
 
 
-        contacts.add(new Contact("Jane", "Sanders", "555-1593", "394 E 22nd Ave",
+        contacts.add(new Contact("Bernardo", "O'Higgins", "555-1593", "Santiago de Chile",
                 MonthDay.of(Month.JANUARY, 5),
-                Instant.parse("2013-02-01T21:22:23Z")
+                Instant.parse("1780-02-01T21:22:23Z")
         ));
-        contacts.add(new Contact( "John", "Smith", "555-0712", "315 Maple St",
-                null, Instant.parse("2012-10-15T15:31:17Z")
+        contacts.add(new Contact("José", "de San Martin", "555-0712", "Buenos Aires",
+                null, Instant.parse("1770-10-15T15:31:17Z")
         ));
-        contacts.add(new Contact("Scott", "Johnson", "555-9834", "424 Oak Dr",
+        contacts.add(new Contact("Simón", "Bolívar", "555-9834", "Caracas",
                 MonthDay.of(Month.NOVEMBER, 17),
-                Instant.parse("2013-04-05T01:45:01Z")
+                Instant.parse("1783-12-17T01:45:01Z")
         ));
     }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException
-    {
-        if(request.getParameter("empty") != null)
+            throws ServletException, IOException {
+        if (request.getParameter("empty") != null)
             request.setAttribute("contacts", Collections.<Contact>emptySet());
         else
             request.setAttribute("contacts", contacts);
